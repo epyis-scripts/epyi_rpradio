315338da7935ec9c44dbf2230f9c805829f2cc0c
Epyi <epyi.contact@gmail.com>
2023-07-02 11:03:48 +0200

    🛠️ Update 02/07/2023 11:03
    🛠️ Fix version checker

--- server/sv_verif.lua
+++ server/sv_verif.lua
@@ -9,8 +9,8 @@ Citizen.CreateThread( function()
             print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
             print("\n###############################")
         elseif tonumber(curVersion) > tonumber(responseText) then
-            print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update.")
+            print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update (or downgrade ?)")
         end
     end
-    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
+    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/main/version", checkVersion, "GET")
 end)
