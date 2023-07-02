5165ffa39b2a216e72d465b539dcecf31e753efc
Epyi <epyi.contact@gmail.com>
2023-07-02 10:11:52 +0200

    🛠️ Update 02/07/2023 10:11
    ✅ Add RageUI Lib
    🛠️ Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,6 +18,16 @@ shared_scripts {
     "@es_extended/imports.lua"
 }
 client_scripts {
+    "RageUI/RMenu.lua",
+    "RageUI/menu/RageUI.lua",
+    "RageUI/menu/Menu.lua",
+    "RageUI/menu/MenuController.lua",
+    "RageUI/components/*.lua",
+    "RageUI/menu/elements/*.lua",
+    "RageUI/menu/items/*.lua",
+    "RageUI/menu/panels/*.lua",
+    "RageUI/menu/windows/*.lua",
+    "RageUI/menu/Function.lua",
 	"client/cl_main.lua",
     "client/cl_functions.lua"
 }
