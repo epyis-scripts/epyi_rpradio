1e4811ba9abc6de064e170b498083dd641e7024d
Epyi <epyi.contact@gmail.com>
2023-07-15 13:21:46 +0200

    Update(sv_utils) Add checker to avoid errors

--- server/sv_utils.lua
+++ server/sv_utils.lua
@@ -1,10 +1,14 @@
 -- # // HASITEM CALLBACK \\ # --
 ESX.RegisterServerCallback("epyi_rpradio:hasItem", function(source, cb, itemName, itemCount)
 	local xPlayer = ESX.GetPlayerFromId(source)
-    local item = xPlayer.hasItem(itemName)
-    if item.count >= itemCount then
-        cb(true)
-    else
-        cb(false)
-    end
+	local item = xPlayer.hasItem(itemName)
+	if not item then
+		cb(false)
+		return
+	end
+	if item.count < itemCount then
+		cb(false)
+		return
+	end
+	cb(true)
 end)
