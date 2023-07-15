5ed8cf1bd37781c345e3ab3c4d7355126fbf85ca
Epyi <epyi.contact@gmail.com>
2023-07-15 16:07:53 +0200

    Update(utils.lua) Move utils to main.lua

--- server/main.lua
+++ server/main.lua
@@ -2,3 +2,14 @@
 ESX.RegisterUsableItem("radio", function(source)
 	TriggerClientEvent("epyi_rpradio:openMenu", source)
 end)
+
+-- # // HASITEM CALLBACK \\ # --
+ESX.RegisterServerCallback("epyi_rpradio:hasItem", function(source, cb, itemName, itemCount)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local item = xPlayer.hasItem(itemName)
+	if not item or item.count < itemCount then
+		cb(false)
+		return
+	end
+	cb(true)
+end)
