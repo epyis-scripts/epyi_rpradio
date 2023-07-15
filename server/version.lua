07c02bb61379c8cfab4fc50c03cdfae6430f5bf3
Epyi <epyi.contact@gmail.com>
2023-07-15 16:01:48 +0200

    Update(server/version.lua) Update the version checker

--- server/version.lua
+++ server/version.lua
@@ -2,23 +2,23 @@
 Citizen.CreateThread(function()
 	function checkVersion(err, responseText, headers)
 		curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
-		if curVersion ~= nil then
-			if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-				print(
-					"^5epyi_rpradio ^4is not up to date. The latest release is "
-						.. responseText
-						.. " but you are on release "
-						.. curVersion
-						.. " -- > https://github.com/J4thgit/epyi_rpradio"
-				)
-			elseif tonumber(curVersion) > tonumber(responseText) then
-				print(
-					"^5epyi_rpradio ^4It looks like github is offline. The resource uses github to check if it's up to date. This does not prevent the resource from working."
-				)
-			end
-		else
+		if curVersion == nil then
 			print(
 				"^5epyi_rpradio ^4It looks like your ressource's version checker is broken. If you want to patch this, go download an official release of this script at -- > https://github.com/J4thgit/epyi_rpradio"
+			)
+			return
+		end
+		if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+			print(
+				"^5epyi_rpradio ^4is not up to date. The latest release is "
+					.. responseText
+					.. " but you are on release "
+					.. curVersion
+					.. " -- > https://github.com/J4thgit/epyi_rpradio"
+			)
+		elseif tonumber(curVersion) > tonumber(responseText) then
+			print(
+				"^5epyi_rpradio ^4It looks like github is offline. The resource uses github to check if it's up to date. This does not prevent the resource from working."
 			)
 		end
 	end
