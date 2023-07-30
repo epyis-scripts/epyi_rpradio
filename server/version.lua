e560fdc3d086d0c0155f4f3cbb8d71c5bc4ccb26
Epyi <epyi.contact@gmail.com>
2023-07-30 20:04:53 +0200

    fix(version.lua) Version checker fix

--- server/version.lua
+++ server/version.lua
@@ -1,24 +1,25 @@
--- # // VERSION CHECKER \\ # --
 Citizen.CreateThread(function()
 	function checkVersion(err, responseText, headers)
 		curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
 		if curVersion == nil then
 			print(
-				"^5epyi_rpradio ^4It looks like your ressource's version checker is broken. If you want to patch this, go download an official release of this script at -- > https://github.com/J4thgit/epyi_rpradio"
+				"^5epyi_rpradio ^4It looks like your ressource's version checker is broken. If you want to patch this, go download an official release of this script at -- > https://github.com/epyis-scripts/epyi_rpradio^0"
 			)
 			return
 		end
-		if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+		if responseText == nil then
+			print(
+				"^5epyi_rpradio ^4It looks like github is offline. The resource uses github to check if it's up to date. This does not prevent the resource from working.^0"
+			)
+			return
+		end
+		if curVersion ~= responseText then
 			print(
 				"^5epyi_rpradio ^4is not up to date. The latest release is "
 					.. responseText
 					.. " but you are on release "
 					.. curVersion
-					.. " → https://github.com/epyis-scripts/epyi_rpradio"
-			)
-		elseif tonumber(curVersion) > tonumber(responseText) then
-			print(
-				"^5epyi_rpradio ^4It looks like github is offline. The resource uses github to check if it's up to date. This does not prevent the resource from working."
+					.. " → https://github.com/epyis-scripts/epyi_rpradio^0"
 			)
 		end
 	end
